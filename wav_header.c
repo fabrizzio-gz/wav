0204b222a4dcce065b88a52a6a4d7e1c80e63a8f
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-01-22 22:15:34 -0400

    (feat, refactor) Use struct for header
    Instead of reading each byte and reading them from the file, create a
    `struct header_struct` type which holds the bytes already aligned.

--- wav_header.c
+++ wav_header.c
@@ -2,9 +2,17 @@
 #include <string.h>
 #include <stdlib.h>
 
-void get_4bytes(FILE *fp, char * destination);
+#define HEADER_SIZE 44
 
-/* Prints the hex characters of a file */
+typedef char byte;
+
+struct header_struct {
+  char chunk_id[4];
+  int chunk_size;
+};
+
+
+/* Print the header of a .wav file */
 int main(int argc, char *argv[]) {
   FILE *fp;
   
@@ -20,41 +28,16 @@ int main(int argc, char *argv[]) {
     return 2;
   }
 
-  /* Verify it's a RIFF file: 4 bytes */
-  char chunkId[5];
-  get_4bytes(fp, chunkId);
-  chunkId[5] = '\0';
-    
-  if (strcmp(chunkId, "RIFF") != 0) {
-    printf("Error: File %s isn't in RIFF format\n", argv[1]);
-    return 3;
-  }
+  byte file_bytes[HEADER_SIZE];
+  int i;
+  for (i = 0; i < HEADER_SIZE && (file_bytes[i] = getc(fp)) != EOF; i++)
+    ;
 
-  /* Get file size: 4 bytes */
-  char size_bytes[4];
-  get_4bytes(fp, size_bytes);
-  /* This works only on little endian machines */
-  int *size = &size_bytes;
-  printf("File size is: %d\n", *size + 8);
 
-  /* Verify it's a WAV file  */
-  
+  struct header_struct *header = &file_bytes;
 
-  printf("OK\n");
-  fclose(fp);
-  
+  printf("%.4s\n", header->chunk_id);
+  printf("%d\n", header->chunk_size);
+
   return 0;
-}
-
-void get_4bytes(FILE *fp, char *destination) {
-  int file_char;
-  int c = 0;
-  while (c < 4 && (file_char = getc(fp)) != EOF) 
-    destination[c++] = (char) file_char;
-
-  /* Wasn't able to fetch 4 bytes */
-  if (c != 4) {
-    printf("Invalid file\n");
-    exit(1);
-  }
 }
