e8489dc3c95a84a5d066cc4d11c21203af8082e2
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-01-22 20:15:28 -0400

    Verify <file> is in RIFF format

--- wav_header.c
+++ wav_header.c
@@ -23,36 +23,16 @@ int main(int argc, char *argv[]) {
   int file_char;
   int c = 0;
   while ((file_char = getc(fp)) != EOF && c < 4) 
-    header[c++] = (char) file_char;
+    chunkId[c++] = (char) file_char;
   
   if (strcmp(chunkId, "RIFF") != 0) {
     printf("Error: File %s isn't in RIFF format", argv[1]);
     return 3;
   }
 
-  /* Get file size: 4 bytes */
-  char size_bytes[4];
-  c = 0;
-  while ((file_char = getc(fp)) != EOF && c < 4) 
-    size_bytes[c++] = (char) file_char;
-  int *size = (int) &size_bytes;
-
-  printf("Size is: %d", *size);
-
   printf("OK\n");
   fclose(fp);
   
   return 0;
-}
-
-
-void print_line_chars(char *chars) {
-  printf("|%s|\n", chars);
-}
-
-
-int is_printable(char c) {
-  /* Check if c is between printable ASCII chars */
-  return c >= 32 && c <= 126;
 }
 
