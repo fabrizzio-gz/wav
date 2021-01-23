40a7740081d5a4f186324277c3a3fe7b21e425a6
fabrizzio-gz <fabrizzio.gz@gmail.com>
2021-01-22 22:23:55 -0400

    Complete all fields on header struct

--- wav_header.c
+++ wav_header.c
@@ -9,6 +9,17 @@ typedef char byte;
 struct header_struct {
   char chunk_id[4];
   int chunk_size;
+  char format[4];
+  char subchunk1_id[4];
+  int subchunk1_size;
+  short audio_format;
+  short num_channels;
+  int sample_rate;
+  int byte_rate;
+  short block_align;
+  short bits_per_sample;
+  char subchunk2_id[4];
+  int subchunk2_size;
 };
 
 
@@ -33,11 +44,21 @@ int main(int argc, char *argv[]) {
   for (i = 0; i < HEADER_SIZE && (file_bytes[i] = getc(fp)) != EOF; i++)
     ;
 
-
   struct header_struct *header = &file_bytes;
 
   printf("%.4s\n", header->chunk_id);
   printf("%d\n", header->chunk_size);
+  printf("%.4s\n", header->format);
+  printf("%.4s\n", header->subchunk1_id);
+  printf("%d\n", header->subchunk1_size);
+  printf("%d\n", header->audio_format);
+  printf("%d\n", header->num_channels);
+  printf("%d\n", header->sample_rate);
+  printf("%d\n", header->byte_rate);
+  printf("%d\n", header->block_align);
+  printf("%d\n", header->bits_per_sample);
+  printf("%.4s\n", header->subchunk2_id);
+  printf("%d\n", header->subchunk2_size);
 
   return 0;
 }
